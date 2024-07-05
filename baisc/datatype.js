01ca31879f81663c5fd07ca6ff6d5bbaa94036ee
priyankabisht21053 <priyankabisht21053.in>
2024-07-05 07:46:47 +0530

    first commit

---
+++ baisc/datatype.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs, not brower
+
+console.log(3+3);
+ console.log("priynaka")
+
+
+//number =>2 to power 53
+//bigint 
+//string
+//let
+//const
+//boolean =>true/ false
+// null =>standalone value
+//undefined =>
+//symbol => for unique
+//object
+console.log( typeof null);
---
+++ baisc/first.js
@@ -0,0 +1,18 @@
+const accountId =12345
+let accountEmail = "priyanka12@gamil.com"
+var accountPassword ="1234"
+accountCity = "Delhi"
+let accountstate;
+
+
+// accountId =2
+
+console.log(accountId);
+
+accountEmail="hc@gmainl"
+
+accountPassword = "2367567"
+accountCity= "uttrakhand"
+
+console.log(accountEmail);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountstate]);
