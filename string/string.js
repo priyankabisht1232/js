8015ba0f22d6332c90d603043f3c739e0e4ca2c7
priyankabisht21053 <priyankabisht21053.in>
2024-07-05 15:08:42 +0530

    chanfe is done

---
+++ string/string.js
@@ -0,0 +1,29 @@
+const  name= "priyanka"
+const repocount = 50;
+
+// console.log(name +repocount + "value");
+
+
+console.log(`hello my name is ${name} and my repo Count is ${repocount}`);
+     gameName = new String('priyanka-bisht')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.indexOf("k"));
+// console.log(gameName.toLocaleUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.substr());
+// console.log(gameName.substring());
+
+ gameName = gameName.substring(0,4);
+ console.log(newstring);
+
+ const newString = gameName.substring(-8,4);
+ console.log(newString);
+
+ const anotherString = gameName.slice(-8,4)
+ console.log(anotherString);
+
